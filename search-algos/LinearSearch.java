268b74926aadc9eb4fe5cd51f63395f1f3a56590
Sairam Harikrishnan <sai_hari98@hotmail.com>
2022-04-03 18:25:37 +0530

    Linear search improved

--- search-algos/LinearSearch.java
+++ search-algos/LinearSearch.java
@@ -13,11 +13,31 @@ public class LinearSearch{
         return elementPosition;
     }
 
+    // If element is in last index, it will take O(n). We can reduce this to O(1).
+    //Worst case will be O(n/2)
+    private static int linearSearchOptimized(int[] elements, int elementToSearch){
+        //-1 means element not present
+        int elementPosition = -1;
+        int left = 0;
+        int right = elements.length - 1;
+        for(; left <= right; left++, right--){
+            if(elements[left] == elementToSearch){
+                elementPosition = left+1;
+                break;
+            }
+            if(elements[right] == elementToSearch){
+                elementPosition = right+1;
+                break;
+            }
+        }
+        return elementPosition;
+    }
+
     public static void main(String[] args){
         int[] elements = {10,58,45,30,36,85,74,143,100,111};
         //fetching input element from program arguments
         int elementToSearch = Integer.parseInt(args[0]);
-        int elementPosition = linearSearch(elements, elementToSearch);
+        int elementPosition = linearSearchOptimized(elements, elementToSearch);
         System.out.println("Element present at position: "+elementPosition);
     }
 }
