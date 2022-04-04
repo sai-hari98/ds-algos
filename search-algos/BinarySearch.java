e6afba3858bd8b3c4cfe10628776ac05a61308e2
Sairam Harikrishnan <sai_hari98@hotmail.com>
2022-04-04 23:11:03 +0530

    Binary search algorithm

---
+++ search-algos/BinarySearch.java
@@ -0,0 +1,42 @@
+public class BinarySearch {
+
+    //Binary search applies only for sorted list/array
+    public static int binarySearch(int[] elements, int elementToSearch, int start, int end){
+        int middlePosition = (start+end)/2;
+        if(start > end)
+            return -1;
+
+        if(elements[middlePosition] == elementToSearch){
+            return middlePosition+1;
+        }else if(elements[middlePosition] > elementToSearch){
+            return binarySearch(elements, elementToSearch, 0, middlePosition-1);
+        }else{
+            return binarySearch(elements, elementToSearch, middlePosition+1, end);
+        }
+    }
+
+    public static int binarySearchIterative(int[] elements, int elementToSearch){
+        int start = 0;
+        int end = elements.length - 1;
+
+        while(start<=end){
+            int middle = (start+end)/2;
+            if(elements[middle] == elementToSearch)
+                return middle+1;
+
+            if(elements[middle] > elementToSearch)
+                end = middle - 1;
+            else
+                start = middle+1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int[] elements = {5,18,24,36,47,51,54,62,79,83,90,101};
+        int elementToSearch = Integer.parseInt(args[0]);
+        int elementPosition = binarySearchIterative(elements, elementToSearch);
+        System.out.println("Element found at position: " + elementPosition);
+    }
+}
