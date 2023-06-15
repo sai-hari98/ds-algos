c9877f3ab387893d876e6212b657b1931727a0cc
Sairam Harikrishnan <sai_hari98@hotmail.com>
2023-06-15 10:23:17 -0500

    efficient approach to length last word

--- leetcode/strings/LengthLastWord.java
+++ leetcode/strings/LengthLastWord.java
@@ -13,7 +13,21 @@ public class LengthLastWord {
     public static void main(String[] args) {
         new LengthLastWord().lengthOfLastWord("Hello World");
     }
+
+    /* 0ms beats 100% */
     public int lengthOfLastWord(String s) {
+        int i = s.length()-1, length=0;
+        while(i>=0 && s.charAt(i) == ' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i) != ' '){
+            length++;
+            i--;
+        }
+        return length;
+    }
+
+    public int lengthOfLastWordApproach1(String s) {
         s = s.trim();
         String[] words = s.split(" ");
         return words[words.length-1].length();
